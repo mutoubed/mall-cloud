535b50228275950c565202646b7ea8b6dda918dd
yujianchun <jinyanjj@qq.com>
2020-04-15 15:41:05 +0800

    增加网关部分配置属性和熔断功能

---
+++ mall-cloud/mall-gateway/src/main/java/com/guodaye/mall/controller/GatewayController.java
@@ -0,0 +1,12 @@
+package com.guodaye.mall.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GatewayController {
+    @GetMapping("/fallback")
+    public String fallback(){
+        return "it's missing";
+    }
+}
